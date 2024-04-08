77de859661ecb149159e884350eff7f8eae4cf24
drisyx <drisyashenoyd@gmail.com>
2024-04-08 20:08:02 +0530

    AddBus completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AddBus from './components/AddBus';
 
 function App() {
   return (
     <div >
-      
+      <AddBus/>
     </div>
   );
 }
