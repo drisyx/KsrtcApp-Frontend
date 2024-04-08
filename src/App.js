bc32705dead37227cff8070c5432947be79c9238
drisyx <drisyashenoyd@gmail.com>
2024-04-08 21:28:55 +0530

    ViewAll completed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import AddBus from './components/AddBus';
 import DeleteBus from './components/DeleteBus';
 import SearchBus from './components/SearchBus';
 import AppHeader from './components/AppHeader';
+import ViewallBus from './components/ViewallBus';
 
 function App() {
   return (
     <div >
       <AppHeader/>
+      <ViewallBus/>
       <AddBus/>
       <DeleteBus/>
       <SearchBus/>
---
+++ src/components/ViewallBus.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+
+const ViewallBus = () => {
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                                <div class="card" >
+
+                                    <div class="card-body">
+                                        <h5 class="card-title">KSRTC SWIFT-HYBRID AC SEATER CUM SLEEPER</h5>
+                                        <p class="card-text">Trivandrum to Bangalore via Kottayam-Angamaly-Thrissur-Palakkad-Salem</p>
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">Journey time 16:35hrs</li>
+                                        <li class="list-group-item">KL07 AG 2043</li>
+                                        <li class="list-group-item">Ramanadhan Nair</li>
+                                    </ul>
+                                    <div class="card-body">
+                                        <a href="#" class="card-link">View</a>
+                                        <a href="#" class="card-link">Book a seat</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                                <div class="card" >
+
+                                    <div class="card-body">
+                                        <h5 class="card-title">KSRTC AC MULTI AXLE</h5>
+                                        <p class="card-text">Chennai to Eranakulam via Chengalpattu-Tindivanam-Salem-Coimbatore-Palakkad-Thrissur-Angamaly-Vytilla Hub</p>
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">Journey time 12:55hrs</li>
+                                        <li class="list-group-item">KL32 DA 7453</li>
+                                        <li class="list-group-item">Lokesh V</li>
+                                    </ul>
+                                    <div class="card-body">
+                                        <a href="#" class="card-link">View</a>
+                                        <a href="#" class="card-link">Book a seat</a>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                                <div class="card" >
+
+                                    <div class="card-body">
+                                        <h5 class="card-title">KSRTC SWIFT-DELUXE AIR BUS</h5>
+                                        <p class="card-text">Thrissur to Ooty via Venjarammoodu-Adoor-Chengannur-Kottayam-Angamaly-Thrissur-Naduvattam</p>
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">Journey time 4:50hrs</li>
+                                        <li class="list-group-item">KL12 V 6754</li>
+                                        <li class="list-group-item">Bargavan T K</li>
+                                    </ul>
+                                    <div class="card-body">
+                                        <a href="#" class="card-link">View</a>
+                                        <a href="#" class="card-link">Book a seat</a>
+                                    </div>
+                                </div>
+
+
+
+
+
+                            </div>
+
+
+
+
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                                <div class="card" >
+
+                                    <div class="card-body">
+                                        <h5 class="card-title">KSRTC SWIFT-GARUDA A/C SEATER BUS</h5>
+                                        <p class="card-text">Pondicherry to Kannur via Cuddalore-Salem-Palakkad-Malappuram-Kozhikode-Thalassery</p>
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">Journey time 13:45hrs</li>
+                                        <li class="list-group-item">KL09 BG 8645</li>
+                                        <li class="list-group-item">Tony Skaria</li>
+                                    </ul>
+                                    <div class="card-body">
+                                        <a href="#" class="card-link">View</a>
+                                        <a href="#" class="card-link">Book a seat</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default ViewallBus
