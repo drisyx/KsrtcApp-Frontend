35ff294d5e40ef648bfaa9bf9d067e81b02c2e5e
drisyx <drisyashenoyd@gmail.com>
2024-04-08 20:16:31 +0530

    DeleteBus completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddBus from './components/AddBus';
+import DeleteBus from './components/DeleteBus';
 
 function App() {
   return (
     <div >
       <AddBus/>
+      <DeleteBus/>
     </div>
   );
 }
